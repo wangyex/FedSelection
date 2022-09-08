e6e466aa5d956fcc348f2e32fab67e4fa89c32bd
“Ye <zxcsef@msn.com>
2022-09-08 13:09:44 -0400

    Added Plotting for Accuracy and Loss

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import datetime
 import os
 import logging
 import torch, random
+import matplotlib.pyplot as plt
 
 from server import *
 from client import *
@@ -15,7 +16,10 @@ if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Federated Learning')
 	parser.add_argument('-c', '--conf', dest='conf')
 	args = parser.parse_args()
-	
+
+	los = []
+	epoch = []
+	accuracy = []
 
 	with open(args.conf, 'r') as f:
 		conf = json.load(f)	
@@ -31,7 +35,7 @@ if __name__ == '__main__':
 		
 	print("\n\n")
 	for e in range(conf["global_epochs"]):
-	
+		epoch.append(e)
 		candidates = random.sample(clients, conf["k"])
 		
 		weight_accumulator = {}
@@ -49,13 +53,23 @@ if __name__ == '__main__':
 		server.model_aggregate(weight_accumulator)
 		
 		acc, loss = server.model_eval()
-		
+		accuracy.append(acc)
+		los.append(loss)
 		print("Epoch %d, acc: %f, loss: %f\n" % (e, acc, loss))
-				
-			
-		
-		
-	
-		
-		
-	
+
+	print(f'\nPlotting Accuracy vs Epochs')
+	plt.plot(epoch,accuracy, c='r')
+
+	plt.xlabel('Epochs')
+	plt.ylabel('Accuracy')
+	#plt.legend()
+	plt.show()
+
+	plt.plot(epoch,los, c='b')
+
+	plt.xlabel('Epochs')
+	plt.ylabel('Loss')
+	# plt.legend()
+	plt.show()
+
+#python main.py -c ./utils/conf.json
